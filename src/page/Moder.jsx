c4b91954f85ef9e1639f120b123343c6dac49245
bekicom <adelkauz6@gmail.com>
2024-04-03 17:15:11 +0500

    syaft

--- src/page/Moder.jsx
+++ src/page/Moder.jsx
@@ -1,9 +1,196 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import tasdiqlash from "../Assets/tasdiqlash.svg";
+import bekorqilish from "../Assets/bekorqilish.svg";
+import bloklash from "../Assets/bloklash.svg";
 
 export default function Moder() {
-    return (
-        <div>
-moderrr
+  const [announcements, setAnnouncements] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const token = getCookie("token");
+
+    const url = new URL(
+      "https://api.frossh.uz/api/announcement/get-by-moderation"
+    );
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+
+    setLoading(true);
+
+    fetch(url, {
+      method: "GET",
+      headers,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setAnnouncements(data.result.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  const getCookie = (name) => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  };
+  // reject function
+  const reject = (id) => {
+    const url = new URL(`https://api.frossh.uz/api/announcement/reject/${id}`);
+
+    const token = getCookie("token");
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+
+    setLoading(true);
+
+    fetch(url, {
+      method: "GET",
+      headers,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        fetchData();
+      })
+      .catch((error) => {
+        console.error("Xato:", error);
+        setLoading(false);
+      });
+  };
+  // accept function
+  const accept = (id) => {
+    const url = new URL(`https://api.frossh.uz/api/announcement/accept/${id}`);
+
+    const token = getCookie("token");
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+
+    setLoading(true);
+
+    fetch(url, {
+      method: "GET",
+      headers,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        fetchData();
+      })
+      .catch((error) => {
+        console.error("Xato:", error);
+        setLoading(false);
+      });
+  };
+
+
+// block function
+const block = (id) => {
+
+    const url = new URL(`https://api.frossh.uz/api/announcement/block/${id}`);
+
+    const token = getCookie("token");
+    const headers = {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    };
+
+    setLoading(true);
+
+    fetch(url, {
+        method: "GET",
+        headers,
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        setLoading(false);
+        fetchData(); // Yangi malumotlarni olish
+    })
+    .catch(error => {
+        console.error('Xato:', error);
+        setLoading(false);
+    });
+};
+
+
+
+  const fetchData = () => {
+    const token = getCookie("token");
+
+    const url = new URL(
+      "https://api.frossh.uz/api/announcement/get-by-moderation"
+    );
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+
+    setLoading(true);
+
+    fetch(url, {
+      method: "GET",
+      headers,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setAnnouncements(data.result.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div className="moder">
+      {loading && <div className="loader"></div>}
+      {announcements.map((item, index) => (
+        <div className="card" key={index}>
+          {item.images && item.images.length > 0 && (
+            <img src={`https://api.frossh.uz/${item.images[0].path}`} alt="" />
+          )}
+          <p>{item.price_uzs_formatted}UZS</p>
+          <p>{item.slug}</p>
+          <span>foydalanuvchi malumotlari:</span>
+          <span>{item.user.phone_number}</span>
+          <div className="controler">
+            <div className="last">
+              <span>{item.user.last_name}</span>
+              <span>{item.address}</span>
+            </div>
+            <div className="control">
+              <button onClick={() => accept(item.id)}>
+                <img src={tasdiqlash} alt="" />
+              </button>
+              <button onClick={() => reject(item.id)}>
+                <img src={bekorqilish} alt="" />
+              </button>
+              <button   onClick={()=> block(item.id)}  >
+                <img src={bloklash} alt="" />
+              </button>
+            </div>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
--- src/page/ModerLogin.jsx
+++ src/page/ModerLogin.jsx
@@ -1,74 +1,53 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import OtpInput from "react-otp-input";
+import { useNavigate } from "react-router-dom";
 
 export default function ModerLogin() {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [otp, setOtp] = useState("");
   const [modal, setModal] = useState(false);
-  console.log(modal);
+  const navigate = useNavigate();
+  const [seconds, setSeconds] = useState(0);
+  // sms ni qayta yuborish
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(prevSeconds => (prevSeconds === 0 ? 59 : prevSeconds - 1)); 
+    }, 1000);
+  
+    if (seconds === 0) {
+      clearInterval(interval);
+    }
+  
+    return () => clearInterval(interval);
+  }, [seconds]);
+  
 
 
+  // vaue
   const handlePhoneNumberChange = (event) => {
     setPhoneNumber(event.target.value);
   };
 
   // sms submit
+  const handlesmssubmit = () => {
+    setModal(false);
+    navigate("/moder");
 
-// sms submit
-const handlesmssubmit = () => {
-  setModal(false); // Modalni yopish
+    const url = new URL("https://api.frossh.uz/api/auth/verify");
 
-  const url = new URL("https://api.frossh.uz/api/auth/verify");
-
-  const headers = {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-  };
-
-  // Telefon raqamini to'g'ri formatga o'tkazish
-  const formattedPhoneNumber = JSON.parse(localStorage.getItem('tel'))
-  console.log(formattedPhoneNumber);
-
-  const body = {
-      phone_number: formattedPhoneNumber, // Modal oynasidan olgan telefon raqami
-      code: otp, // Modal oynasidan olgan kod
-  };
-
-  fetch(url, {
-      method: "POST",
-      headers,
-      body: JSON.stringify(body),
-  })
-      .then((response) => response.json())
-      .then((data) => {
-          // Ma'lumotlarni olinganidan so'ng qanday ishlashni yozing
-          console.log(data);
-          
-      })
-      .catch((error) => {
-          // Xato bo'lganda qanday ishlashni yozing
-          console.error("Xato:", error);
-      });
-};
-
-
-    const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const url = "https://api.frossh.uz/api/auth/login";
     const headers = {
       "Content-Type": "application/json",
       Accept: "application/json",
     };
 
-    // Telefon raqamini to'g'ri formatga o'tkazish
-    const formattedPhoneNumber = `${phoneNumber}`;
+    const formattedPhoneNumber = JSON.parse(localStorage.getItem("tel"));
+    console.log(formattedPhoneNumber);
 
     const body = {
       phone_number: formattedPhoneNumber,
+      code: otp,
     };
-
-    localStorage.setItem('tel', JSON.stringify(formattedPhoneNumber))
 
     fetch(url, {
       method: "POST",
@@ -77,10 +56,50 @@ const handlesmssubmit = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data); // Ma'lumotlarni ko'rish
+        console.log(data); 
+        const token = data.result.token;
+        
+        const tokenWithoutId = token.split('|').slice(1).join('|');
+        console.log(tokenWithoutId); 
+    
+        document.cookie = `token=${tokenWithoutId}; expires=Fri, 31 Dec 9999 23:59:59 GMT`; // Cookie muddati o'zgartirilishi mumkin
+    })
+    
+    
+
+      .catch((error) => {
+        console.error("Xato:", error);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const url = "https://api.frossh.uz/api/auth/login";
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+
+    const formattedPhoneNumber = `${phoneNumber}`;
+
+    const body = {
+      phone_number: formattedPhoneNumber,
+    };
+
+    localStorage.setItem("tel", JSON.stringify(formattedPhoneNumber));
+
+    fetch(url, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
       })
       .catch((error) => {
-        console.error("Xatolik:", error); // Xatolikni ko'rish
+        console.error("Xatolik:", error);
       });
 
     setPhoneNumber("");
@@ -110,24 +129,30 @@ const handlesmssubmit = () => {
           value={otp}
           onChange={setOtp}
           numInputs={6}
-          renderSeparator={<span style={{ margin: "0 5px" }}> - </span>}
+          renderSeparator={<span style={{ margin: "0 5px" }}>  </span>}
           renderInput={(props, index) => (
             <input
-              {...props}
-              key={index}
-              style={{
-                width: "80px",
-                height: "80px",
-                textAlign: "center",
-                border: "1px solid black",
-                borderRadius: "5px",
-                margin: "0 5px",
-                fontSize: "23px",
-              }}
-              
-            />
+            {...props}
+            key={index}
+            style={{
+              width: "80px",
+              height: "80px",
+              textAlign: "center",
+              border: `2px solid ${seconds === 0 ? "red" : "black"}`, // Set red border if seconds is 0
+              borderRadius: "5px",
+              margin: "0 5px",
+              fontSize: "23px",
+              borderRadius: "12px", 
+            }}
+          />
           )}
+          
         />
+         <div className="qaytayuborish">
+          <span  style={ seconds === 0 ? {color:"red"} : {color:"black"} }    >00:{seconds}</span> <span>Kod kelmadimi?</span>
+          <span className="qayta" >Qayta yuborish</span>
+        </div>
+
 
         <button onClick={handlesmssubmit}>Yuborish</button>
       </div>
